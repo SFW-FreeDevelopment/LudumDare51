46d4e185ee6693fd064c42d00b3b50797e0cbce1
Steven Garberg <90432050+StevenGarberg@users.noreply.github.com>
2022-10-01 14:38:20 -0400

    LD51-23 Create game scene (#8)
    * Added spawner
    
    * Working on player controller

--- Unity/Assets/Scripts/Controllers/PlayerController.cs
+++ Unity/Assets/Scripts/Controllers/PlayerController.cs
@@ -12,8 +12,42 @@ namespace LD51.Unity.Controllers
         private float _nextShotAvailableAt = 0;
         private bool NextShotAvailable => Time.time >= _nextShotAvailableAt;
 
+        [SerializeField] private float _movementForce = 10.0f;
+        private Rigidbody2D _rigidbody2D;
+        private bool _isMoving = false;
+        
+        private void Awake()
+        {
+            _rigidbody2D = GetComponent<Rigidbody2D>();
+        }
+        
         private void Update()
         {
+            if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow))
+            {
+                _rigidbody2D.AddForce(Vector2.left * Time.deltaTime * _movementForce, ForceMode2D.Impulse);
+                _isMoving = true;
+            }
+            else if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow))
+            {
+                _rigidbody2D.AddForce(Vector2.right * Time.deltaTime * _movementForce, ForceMode2D.Impulse);
+                _isMoving = true;
+            }
+            else if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.UpArrow))
+            {
+                _rigidbody2D.AddForce(Vector2.up * Time.deltaTime * _movementForce, ForceMode2D.Impulse);
+                _isMoving = true;
+            }
+            else if (Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.DownArrow))
+            {
+                _rigidbody2D.AddForce(Vector2.down * Time.deltaTime * _movementForce, ForceMode2D.Impulse);
+                _isMoving = true;
+            }
+            else
+            {
+                _isMoving = false;
+            }
+            
             if (Input.GetMouseButton(0) && NextShotAvailable)
             {
                 _nextShotAvailableAt = Time.time + IntervalBetweenShots;
