2974d732f89742da88ce6dec1b8cbcd6ddf86565
Steven Garberg <steven.garberg.jr@gmail.com>
2022-10-01 00:26:03 -0400

    Update

--- Unity/Assets/Scripts/PlayerController.cs
+++ Unity/Assets/Scripts/PlayerController.cs
@@ -1,18 +1,25 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public SpriteRenderer Target;
+    
+    private void Update()
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        // TODO: Get current mouse position to determine target position
+        Vector2 mouse = new Vector2(Input.mousePosition.x,Input.mousePosition.y);
+        Ray ray;
+        ray = Camera.main.ScreenPointToRay(mouse);
+        RaycastHit hit;
+         
+        if(Physics.Raycast(ray,out hit, 10))
+        {
+            Target.transform.position = hit.point;
+            
+            if(hit.point.x < transform.position.x)
+                Debug.Log("Left");
+            else
+                Debug.Log("Right");
+        }
     }
 }
