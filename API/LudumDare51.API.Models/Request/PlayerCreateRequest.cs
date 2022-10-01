fea562690881ae82eb00417b952d3cf6728aed92
Jordan Garcia <jordan.garcia.roche@gmail.com>
2022-10-01 00:32:58 -0400

    Create PlayerRequest Model

---
+++ API/LudumDare51.API.Models/Request/PlayerCreateRequest.cs
@@ -0,0 +1,8 @@
+namespace LudumDare51.API.Models.Request;
+
+public class PlayerCreateRequest
+{
+    public string DisplayName { get; set; }
+    public string ShirtColor { get; set; }
+    public string PantColor { get; set; }
+}
