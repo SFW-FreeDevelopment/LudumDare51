486703df336130efd97354215ec12fb0244bce1b
Jordan Garcia <jordan.garcia.roche@gmail.com>
2022-10-01 22:37:59 -0400

    hooked PlayerController into PlayerRepository (#16)
    * hooked PlayerController into PlayerRepository
    
    * stashed Mongo API key

--- API/LudumDare51.API.Models/Player.cs
+++ API/LudumDare51.API.Models/Player.cs
@@ -1,4 +1,6 @@
-﻿namespace LudumDare51.API.Models;
+﻿using LudumDare51.API.Models.Request;
+
+namespace LudumDare51.API.Models;
 
 public class Player : BaseResource
 {
@@ -6,6 +8,15 @@ public class Player : BaseResource
     public ushort Wins { get; set; }
     public ushort Losses { get; set; }
     public string ShirtColor { get; set; }
-    public string PaintColor { get; set; }
+    public string PantColor { get; set; }
     public ushort TimesPlayed { get; set; }
+
+    public Player() { }
+
+    public Player(PlayerCreateRequest playerCreateRequest)
+    {
+        DisplayName = playerCreateRequest.DisplayName;
+        ShirtColor = playerCreateRequest.ShirtColor;
+        PantColor = playerCreateRequest.PantColor;
+    }
 }
