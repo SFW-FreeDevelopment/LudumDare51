b078997d80fc108cc57fdb51f8a10612b837f641
Christopher Plummer <chrisplummer0194@gmail.com>
2022-10-01 23:31:10 -0400

    Defined Player in .NET backend for Angular

---
+++ API/LudumDare51.Leaderboard/Models/Player.cs
@@ -0,0 +1,12 @@
+﻿namespace LudumDare51.Leaderboard.Models;
+
+public class Player
+{
+    public string Id { get; set; }
+
+    public string DisplayName { get; set; }
+
+    public ushort Score { get; set; }
+        
+    public ushort Waves { get; set; }
+}
