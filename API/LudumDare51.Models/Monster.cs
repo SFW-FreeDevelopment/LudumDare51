bacf755c7a06303a8eb0c08bb33cea67b81b3552
John Teaff <johnteaff@gmail.com>
2022-10-01 20:34:53 -0400

    Add monster game model

---
+++ API/LudumDare51.Models/Monster.cs
@@ -0,0 +1,9 @@
+﻿namespace LudumDare51.Models
+{
+    public class Monster
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public ushort Health { get; set; }
+    }
+}
