5cbc8007683a90ab618d95d2a169b99117a969d1
Christopher Plummer <chrisplummer0194@gmail.com>
2022-10-02 23:37:53 -0400

    Logging

--- API/LudumDare51.API/Controllers/PlayerController.cs
+++ API/LudumDare51.API/Controllers/PlayerController.cs
@@ -51,7 +51,8 @@ namespace LudumDare51.API.Controllers
         [SwaggerResponse(StatusCodes.Status400BadRequest, null)]
         public async Task<IActionResult> ProcessGameResults([FromRoute] string id, [FromBody] ProcessGameResultsRequest request)
         {
-            _logger.Log(LogLevel.Information, $"Id: {id}{Environment.NewLine}Request: {JsonSerializer.Serialize(request)}");
+            var requestJson = JsonSerializer.Serialize(request);
+            _logger.LogTrace("Id: {id}\nRequest: {requestJson}", id, requestJson);
             var player = await _playerRepository.Get(id);
             UpdatePlayerStats(player, request);
             return Ok(await _playerRepository.Update(id, player));
